3c356dbbfb165ccb3ce896b9506280b70c2b3402
erickmx <erickblanc96@gmail.com>
2019-03-17 20:22:33 -0600

    arreglado el color picker

--- src/components/FormMap/FormMap.js
+++ src/components/FormMap/FormMap.js
@@ -1,75 +1,92 @@
 import React, { Component } from "react";
-import { ChromePicker, SketchPicker } from 'react-color'
+import { ChromePicker } from "react-color";
 import { inject, observer } from "mobx-react";
 import "./FormMap.scss";
 
 const ColorPicker = ({
-  onClick, color, displayColorPicker, onClose, onChange
+  onClick,
+  color,
+  displayColorPicker,
+  onClose,
+  onChange,
+  ...props
 }) => {
-
   return (
     <div>
       <div className="swatch" onClick={() => onClick()}>
-        <div className="color" style={{ backgroundColor: this.props.color }} />
+        <div className="color" style={{ backgroundColor: color }} />
       </div>
-      {displayColorPicker ? <div className="popover">
-        <div className="cover" onClick={() => onClose()} />
-        <ChromePicker color={color} onChange={(colorSelected) => onChange(colorSelected)} />
-      </div> : null}
-
+      {displayColorPicker ? (
+        <div className="popover">
+          <div className="cover" onClick={() => onClose()} />
+          <ChromePicker
+            color={color}
+            onChange={colorSelected => onChange(colorSelected)}
+          />
+        </div>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
 @inject("maps")
 @observer
 class FormMap extends Component {
-
   state = {
     selectedFile: null,
-    idShow: -1,
-
-  }
+    idShow: -1
+  };
 
   handleColorChange = (id, color) => {
-    this.props.maps.setTexture(id, color)
-  }
+    this.props.maps.setTexture(id, color);
+  };
 
-  handleClick = (idShow) => {
-    this.setState({ idShow })
+  handleClick = idShow => {
+    this.setState({ idShow });
   };
 
   handleClose = () => {
-    this.setState({ idShow: -1 })
+    this.setState({ idShow: -1 });
   };
 
   render() {
     return (
       <div>
-        <input type="file" id="file" onChange={event => {
-          const file = event.target.files && event.target.files[0];
-          this.setState({
-            selectedFile: file
-          }, () => {
-            console.log(file);
-            this.props.maps.setMap([[1, 2, 3], [4, 5, 6]])
-          })
-        }} />
+        <input
+          type="file"
+          id="file"
+          onChange={event => {
+            const file = event.target.files && event.target.files[0];
+            this.setState(
+              {
+                selectedFile: file
+              },
+              () => {
+                console.log(file);
+                this.props.maps.setMap([[1, 2, 3], [4, 5, 6]]);
+              }
+            );
+          }}
+        />
         <ul>
-          {!!this.state.selectedFile && this.props.maps.idList.map((idMap, index) => {
-            console.log(idMap);
-            return (
-              <li>
-                <p>ID: {idMap}</p>
-                <ColorPicker
-                  onClose={this.handleclose}
-                  displayColorPicker={this.state.idShow === index}
-                  onClick={() => this.handleClick(index)}
-                  color={this.props.maps.textures[idMap]}
-                  onChange={(color) => this.handleColorChange(idMap, color.hex)} />
-              </li>
-            )
-          })}
+          {!!this.state.selectedFile &&
+            this.props.maps.idList.map((idMap, index) => {
+              return (
+                <li>
+                  <p>ID: {idMap}</p>
+                  <ColorPicker
+                    onClose={this.handleClose}
+                    displayColorPicker={this.state.idShow === index}
+                    onClick={() => this.handleClick(index)}
+                    color={
+                      this.props.maps.textures &&
+                      this.props.maps.textures[idMap]
+                    }
+                    onChange={color => this.handleColorChange(idMap, color.hex)}
+                  />
+                </li>
+              );
+            })}
         </ul>
         <button disabled={!this.state.selectedFile}>Confirmar</button>
       </div>
@@ -78,5 +95,3 @@ class FormMap extends Component {
 }
 
 export { FormMap };
-
-
